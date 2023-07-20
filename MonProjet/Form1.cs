46103c85645f8445f0102e3614da59cba57db086
Eridwyn <Markchavatte@gmail.com>
2023-07-20 22:51:05 +0200

    newtest

---
+++ MonProjet/Form1.cs
@@ -0,0 +1,62 @@
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MonProjet
+{
+    public partial class Form1 : Form
+    {
+        private ProgressBar progressBar;
+        private Label messageLabel;
+
+        public Form1()
+        {
+            this.Text = "Sauvegarde en cours";
+            this.Size = new Size(400, 200);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.MinimizeBox = true;
+
+            progressBar = new ProgressBar();
+            progressBar.Location = new Point(20, 60);
+            progressBar.Size = new Size(360, 20);
+            progressBar.Style = ProgressBarStyle.Continuous;
+            progressBar.Minimum = 0;
+            progressBar.Maximum = 100;
+            this.Controls.Add(progressBar);
+
+            messageLabel = new Label();
+            messageLabel.Text = "Sauvegarde en cours...";
+            messageLabel.AutoSize = true;
+            messageLabel.Location = new Point(20, 100);
+            messageLabel.Font = new Font("Segoe UI", 12, FontStyle.Bold);
+            this.Controls.Add(messageLabel);
+
+            var backgroundWorker = new BackgroundWorker();
+            backgroundWorker.DoWork += DoWork;
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        private void DoWork(object? sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i <= 100; i += 10)
+            {
+                UpdateProgress(i);
+                Thread.Sleep(1000);
+            }
+            this.Invoke(new Action(() => this.Close()));
+        }
+
+        private void UpdateProgress(int value)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action<int>(UpdateProgress), new object[] { value });
+                return;
+            }
+
+            progressBar.Value = value;
+        }
+    }
+}
